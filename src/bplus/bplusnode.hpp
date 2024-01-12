2e165164099869fd9bdf020ffc5f4da848725f01
RayBipse <raybipse@gmail.com>
2024-01-11 17:31:13 -0800

    Removed the use of unique_ptr

--- src/bplus/bplusnode.hpp
+++ src/bplus/bplusnode.hpp
@@ -9,7 +9,7 @@ template <class K, class V>
 class BPlusNode {
    public:
     uint16_t key_size;
-    std::unique_ptr<K*> keys;
+    K** keys;
     bool is_leaf;
 };
 
@@ -17,7 +17,7 @@ template <class K, class V>
 class BPlusInternalNode : BPlusNode<K, V> {
    public:
     uint16_t children_size;
-    std::unique_ptr<BPlusNode*> children;
+    BPlusNode** children;
 };
 
 template <class K, class V>

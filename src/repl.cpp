d00224be4d79085f9b4de8ff15023dce3829b433
RayBipse <raybipse@gmail.com>
2024-01-15 16:39:04 -0800

    Added skeleton structure to VM, cmds, meta cmds

--- src/repl.cpp
+++ src/repl.cpp
@@ -2,10 +2,6 @@
 #include <iostream>
 #include <string>
 
-void print_prompt() {
-    std::cout << "db > ";
-}
-
 void read_input(std::string& str) {
     try {
         std::getline(std::cin, str);
@@ -15,16 +11,73 @@ void read_input(std::string& str) {
     }
 }
 
+enum class MetaCmdResult { success, unrecognized };
+
+MetaCmdResult do_meta_cmd(std::string input) {
+    if (input == ".exit") {
+        exit(EXIT_SUCCESS);
+    } else {
+        return MetaCmdResult::unrecognized;
+    }
+}
+
+enum class CmdPrepareResult { success, unrecognized };
+
+enum class StatementType { insert, select };
+struct Statement {
+    StatementType type;
+};
+
+CmdPrepareResult prepare_statement(std::string input, Statement& statement) {
+    if (input.starts_with("insert")) {
+        statement.type = StatementType::insert;
+        return CmdPrepareResult::success;
+    }
+    if (input.starts_with("select")) {
+        statement.type = StatementType::select;
+        return CmdPrepareResult::success;
+    }
+    return CmdPrepareResult::unrecognized;
+}
+
+void execute_statement(const Statement& statement) {
+    switch (statement.type) {
+        case (StatementType::insert):
+            std::cout << "insert placeholder\n";
+            break;
+        case (StatementType::select):
+            std::cout << "select placeholder\n";
+            break;
+    }
+}
+
 int main() {
     std::string input;
     while (true) {
-        print_prompt();
+        std::cout << "modeldb > ";
         read_input(input);
-        if (input == ".exit") {
-            break;
+        if (input[0] == '.') {
+            switch (do_meta_cmd(input)) {
+                case MetaCmdResult::success:
+                    continue;
+                case MetaCmdResult::unrecognized:
+                    std::cout << "Unrecognized command \'" << input << "\'.\n";
+                    continue;
+            }
         } else {
-            std::cout << "Unrecognized command \'" << input << "\'.\n";
+            Statement statement;
+            switch (prepare_statement(input, statement)) {
+                case (CmdPrepareResult::success):
+                    break;
+                case (CmdPrepareResult::unrecognized):
+                    std::cout << "Unrecognized keyword at start of \'" << input
+                              << "\'\n";
+                    continue;
+            }
+
+            execute_statement(statement);
+            std::cout << "Executed.\n";
         }
     }
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
 }
